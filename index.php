ca7d39f6c9a28b161fc9e5b958f64200816437f2
Vinicius Lima <vinicius.lima58@aluno.cps.sp.gov.br>
2026-08-18 21:51:59 -0300

    primeiro curriculo Vinicius

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">Deivid</a>
+            <a href="Curriculo_ViniciusPaulino.pdf">Vinicius</a>
+            <a href="">Vitor</a>
+        </nav>
+    </header>
+</body>
+</html>
